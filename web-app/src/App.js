5fee1c4a46db4f6e10606fbf34775116a477cc18
yogesh <kathayat.yogesh@gmail.com>
2019-03-14 09:03:06 +0400

    login component added

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import  Navbar from './components/Navbar'
 import Register from './components/Register';
 import Login from './components/Login';
@@ -12,16 +15,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
   render() {
     return (
-      <Router>
-      <div>
-        <Navbar />
-        <Route exact path="/" component={Home}/>
-        <div className="container">
-        <Route exact path="/register" component={Register}/>
-        <Route exact path="/login" component={Login}/>
-        </div>
-      </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Navbar />
+            <Route exact path="/" component={Home} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
